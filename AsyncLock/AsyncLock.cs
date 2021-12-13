97e62337e13eec94a5dbead35a84de90fe1dedc8
Jeremy Cantu <mail@jeremycantu.com>
2021-12-12 20:21:35 -0600

    init commit with base implementation

---
+++ AsyncLock/AsyncLock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncLock
+{
+    public static class AsyncLock
+    {
+        /// <summary>
+        /// Set limit to a maximum of a single lock
+        /// </summary>
+        private static readonly SemaphoreSlim Lock = new SemaphoreSlim(1, 1);
+
+        public static async Task ExecuteWithLock<T>(Task<Func<T>> f)
+        {
+            // grab the single lock
+            await Lock.WaitAsync();
+
+            try
+            {
+                await f;
+            }
+            catch (Exception ex)
+            {
+                ExceptionDispatchInfo.Capture(ex);
+
+                throw;
+            }
+            finally
+            {
+                Lock.Release();
+            }
+        }
+
+        public static async Task ExecuteWithLock<T>(Func<Task<T>> f)
+        {
+            // grab the single lock
+            await Lock.WaitAsync();
+
+            try
+            {
+                await f();
+            }
+            catch (Exception ex)
+            {
+                ExceptionDispatchInfo.Capture(ex);
+
+                throw;
+            }
+            finally
+            {
+                Lock.Release();
+            }
+        }
+    }
+}
