8c3b3de6b631d5dc4f8669963e512f51b544fbd0
Jeremy Cantu <mail@jeremycantu.com>
2021-12-13 17:40:10 -0600

    Integration test project

---
+++ AsyncLock.Integration.Test/AsyncLockTests.cs
@@ -0,0 +1,58 @@
+#nullable enable
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using NUnit.Framework;
+
+namespace AsyncLock.Integration.Test
+{
+    public class AsyncLockTests
+    {
+        // Internal State
+        private HubConnection? _connection;
+
+        public async Task<HubConnection> GetConnection(string url)
+        {
+            if (_connection == null)
+            {
+                try
+                {
+                    // We check the _connection again after the lock.
+                    // We do this since another call can create it.
+                    if (_connection != null)
+                    {
+                        return _connection;
+                    }
+
+                    _connection = new HubConnectionBuilder()
+                        .WithUrl(url)
+                        .Build();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+
+                    throw;
+                }
+            }
+
+            return _connection;
+        }
+
+        [Test]
+        public async Task AsyncLock_GetConnection_Success_Test()
+        {
+            // arrange
+            HubConnection connection = null;
+
+            // act
+            await AsyncLock.ExecuteWithLock(async () =>
+            {
+                connection = await GetConnection("https://that.place.on.the.internet/hub");
+            });
+
+            // assert
+            Assert.IsNotNull(connection);
+        }
+    }
+}
